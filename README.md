9c141fed98778223c6c34c673eb7b15cd19122c6
Paulo McNally <paulomcnally@gmail.com>
2013-11-21 00:27:27 -0600

    Funciona

---
+++ README.md
@@ -0,0 +1,3 @@
+Votos automáticos en el sitio [http://quierounplanetaverde.com/separtedelasolucion](http://quierounplanetaverde.com/separtedelasolucion)
+
+![Screenshot](http://i.imgur.com/KaFMjwi.jpg)
--- app.js
+++ app.js
@@ -1,62 +1,51 @@
-var http = require('http');
+var http	= require('http');
+var query 	= require('querystring');
+var count = 0;
 
-var querystring = require('querystring');
+function vote() {
 
-var data = querystring.stringify({
-    proyecto_id: 1
-});
-
-var options = {
-    hostname: 'quierounplanetaverde.com',
-    port: 80,
-    path: '/votar-proyecto',
-    method: 'POST',headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(data)
-    }
-};
-
-var req = http.request(options, function(res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-        console.log('BODY: ' + chunk);
-    });
-});
-
-req.on('error', function(e) {
-    console.log('problem with request: ' + e.message);
-});
-
-// write data to request body
-req.write('data\n');
-req.write('data\n');
-req.end();
-
-
-
-
-/*
-
-
-var request = require('request');
-
-var url = 'http://quierounplanetaverde.com/votar-proyecto';
-
-
-
-
-    request.post(url, {form:{"proyecto_id":2}},function (error, response, body) {
-
-        if( error ){
-            console.log( error );
-        }
-        else{
-               console.log( body );
-        }
+    var data = query.stringify({
+        proyecto_id: 2
     });
 
+    var options = {
+        hostname: 'quierounplanetaverde.com'
+        ,port: 80
+        ,path: '/votar-proyecto'
+        ,method: 'POST'
+        ,headers: {
+            'Accept':'application/json, text/javascript, */*; q=0.01'
+            ,'Accept-Encoding':'gzip,deflate,sdch'
+            ,'Accept-Language':'en-US,en;q=0.8,es;q=0.6'
+            ,'Connection':'keep-alive'
+            ,'Content-Length':'13'
+            ,'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'
+            ,'Cookie':'__utma=7833202.1968533372.1383310544.1383310544.1385033544.1; __utmb=7857332.8.10.1383310544; __utmc=7833202; __utmz=7833202.1383310544.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)'
+            ,'Host':'quierounplanetaverde.com'
+            ,'Origin':'http://quierounplanetaverde.com'
+            ,'Referer':'http://quierounplanetaverde.com/separtedelasolucion'
+            ,'User-Agent':'Mozilla/4.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/337.36'
+            ,'X-Requested-With':'XMLHttpRequest'
+        }
+    };
 
-});
-    */
+    var req = http.request(options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            vote();
+            count++;
+            console.log('['+count+'] BODY: ' + chunk);
+        });
+    });
+
+    req.write(data);
+
+    req.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+    });
+
+    req.end();
+
+}
+
+vote();
